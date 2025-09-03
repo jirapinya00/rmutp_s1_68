c63ee218adc9314cba549ad7b64b6f6ba65fe214
jirapinya00 <90089749+jirapinya00@users.noreply.github.com>
2025-09-03 17:25:16 +0700

    ff

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client";  
+import * as bcrypt from "bcrypt";
 
 const app = new Hono();
 const prisma = new PrismaClient();
@@ -12,7 +13,31 @@ app.get("/about", (c) => {
 
 app.get("/profile", async (c) => {               
   const profile = await prisma.profile.findMany(); 
-  return c.json(profile);
+  return c.json({
+        message: "get data completed",
+        data: profile
+    }, 200);
+});
+
+app.post("/profile", async (c) => {
+  const body = await c.req.json();
+
+  const passwordHash = await bcrypt.hash(body.password, 10);
+
+  const result = await prisma.profile.create({
+    data: {
+      username: body.username,
+      password: passwordHash,
+      cardId: body.cardId,
+      mobile: body.mobile,
+      status: false, // ใส่ชัด ๆ ตรงนี้
+    },
+  });
+
+  return c.json({
+    message: "create profile completed",
+    data: result,
+  });
 });
 
 export default app;
--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const hono_1 = require("hono");
 const client_1 = require("@prisma/client");
+const bcrypt = require("bcrypt");
 const app = new hono_1.Hono();
 const prisma = new client_1.PrismaClient();
 app.get("/", (c) => c.text("Hello World"));
@@ -10,6 +11,26 @@ app.get("/about", (c) => {
 });
 app.get("/profile", async (c) => {
     const profile = await prisma.profile.findMany();
-    return c.json(profile);
+    return c.json({
+        message: "get data completed",
+        data: profile
+    }, 200);
+});
+app.post("/profile", async (c) => {
+    const body = await c.req.json();
+    const passwordHash = await bcrypt.hash(body.password, 10);
+    const result = await prisma.profile.create({
+        data: {
+            username: body.username,
+            password: passwordHash,
+            cardId: body.cardId,
+            mobile: body.mobile,
+            status: false, // ใส่ชัด ๆ ตรงนี้
+        },
+    });
+    return c.json({
+        message: "create profile completed",
+        data: result,
+    });
 });
 exports.default = app;
